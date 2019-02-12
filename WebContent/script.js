a91b933501cd3ec350970260f58dc0a7197fbfd5
derekquinn <quinnderek@yahoo.com>
2019-02-12 18:09:36 -0500

    chugging along

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,3 +1,16 @@
-/**
- * 
- */
+// parallel arrays to hold the relevant data
+var beers = [ "Two Hearted", "Fresh Squeezed", "Commodore Perry", "Stone IPA",
+		"Imperial Crème Brûlée", "Lime-a-rita" ];
+var breweries = [ "Bell's", "Deschutes", "Great Lakes", "Stone", "Kuhnhenn",
+		"Bud Light" ];
+var prices = [ 8.00, 7.50, 6.50, 6.20, 8.50, 3.00 ];
+
+// adding items to cart array when clicked
+
+
+// REMOVE ME DEBUG
+
+
+console.log(beers);
+console.log(breweries);
+console.log(prices);
